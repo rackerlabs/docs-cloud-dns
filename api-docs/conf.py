f5383e0eeac210f957343798dcb124b603446992
DonNateR <nate.archer@rackspace.com>
2015-12-07 09:56:46 -0600

    Update Conf.py to prevent GS build errors.

--- api-docs/conf.py
+++ api-docs/conf.py
@@ -85,7 +85,7 @@ today = 'November 3, 2015'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build','common-gs','api-operations/methods*','samples']
+exclude_patterns = ['_build','common-gs','api-operations/methods*','getting-started/examples','samples']
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
