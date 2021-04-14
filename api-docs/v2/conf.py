5fda40795a0714e7d838e6cccc44adaf0c646a85
Ravi <raviteja.valluri@rackspace.com>
2021-04-14 11:39:14 -0700

    HTML for Search, Header and Footer

--- api-docs/v2/conf.py
+++ api-docs/v2/conf.py
@@ -184,10 +184,10 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-html_title = 'Rackspace Managed DNS version 2.0 API Guide'
+# html_title = 'Rackspace Managed DNS version 2.0 API Guide'
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
-html_short_title = 'Managed DNS v2 API Guide'
+# html_short_title = 'Managed DNS v2 API Guide'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
